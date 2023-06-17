5dbb5f9209837e1c4915f3f278aeb6660582855f
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-06-18 05:33:23 +0600

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,19 +1,57 @@
+import { useState, useEffect } from "react";
 import { stats } from "../constants";
 import styles from "../style";
 
-const Stats = () => (
-  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
-    {stats.map((stat) => (
-      <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`} >
+const Stats = () => {
+  const [visitorCount, setVisitorCount] = useState(0);
+  const [totalVisitorCount, setTotalVisitorCount] = useState(0);
+
+  useEffect(() => {
+    // Increment visitor count when the component mounts
+    setVisitorCount((prevCount) => prevCount + 1);
+
+    // Update total visitor count from localStorage
+    const storedTotalCount = localStorage.getItem("totalVisitorCount");
+    if (storedTotalCount) {
+      setTotalVisitorCount(parseInt(storedTotalCount));
+    }
+
+    // Increment total visitor count and update localStorage
+    setTotalVisitorCount((prevCount) => {
+      const newCount = prevCount + 1;
+      localStorage.setItem("totalVisitorCount", newCount.toString());
+      return newCount;
+    });
+  }, []);
+
+  return (
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-15 mb-6`}>
+
+      <div className="flex-1 flex justify-start items-center flex-row m-3">
         <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-white">
-          {stat.value}
+          {totalVisitorCount}+
         </h4>
         <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
-          {stat.title}
+          Visitors
         </p>
       </div>
-    ))}
-  </section>
-);
+
+
+      {stats.map((stat) => (
+        <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`} >
+          <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-white">
+            {stat.value}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+
+
+
+    </section>
+  );
+};
 
 export default Stats;
