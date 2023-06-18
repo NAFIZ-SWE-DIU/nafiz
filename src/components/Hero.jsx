96fc358233ac2c2b044a537d1673d01556706fe3
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-06-18 23:59:51 +0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,33 +9,28 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For{" "}
-            <span className="text-white">1 Month</span> Account
+            <span className="text-white">In the name of Allah,</span> the Most Gracious, the Most Merciful.{" "}
+            {/* <span className="text-white">1 Month</span> Account */}
           </p>
         </div>
 
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[65px] text-[52px] text-white ss:leading-[80.8px] leading-[75px]">
+          When I code, <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">I code with fun. so, I never get bored.</span>{" "}
           </h1>
-          <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
-          </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
-        </h1>
+        {/* <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        I never get bored.
+        </h1> */}
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Our team of experts uses a methodology to identify the credit cards
-          most likely to fit your needs. We examine annual percentage rates,
-          annual fees.
+        "Greetings! This is Nafizul Islam (Noyon), and I am a junior Software Engineer from Dhaka, Bangladesh. I specialize in HTML, CSS, JavaScript, ReactJs, NodeJs, PHP and MySQL Database aiming to develop cutting-edge, user-friendly web solutions that drive business growth. I stay up-to-date with the latest web technologies and strive to deliver exceptional user experiences. Let's collaborate to push the boundaries of web development and create innovative solutions that make a lasting impact."
         </p>
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={robot} alt="billing" className="w-[85%] h-[80%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
